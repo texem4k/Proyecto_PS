a4b14c87f2787d5aade5bf4b6cfa15e34e1b7373
Texenery <texenerybordon123@gmail.com>
2026-03-27 15:40:23 +0000

    test println

--- composeApp/src/jvmMain/kotlin/software/ulpgc/code/main.kt
+++ composeApp/src/jvmMain/kotlin/software/ulpgc/code/main.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
+    println("Hola");
     Window(
         onCloseRequest = ::exitApplication,
         title = "Proyecto_PS",
