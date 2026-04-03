52d560d50ebcc517cc6929cebc1b9cd0e0c37dee
sergtoten <casbaser013@gmail.com>
2026-04-03 13:39:18 +0100

    añadido TaskMonito, PeriodicTaskChecker y PeriodicTaskRenewer

---
+++ composeApp/src/commonMain/kotlin/software/ulpgc/code/architecture/model/TaskMonitor.kt
@@ -0,0 +1,40 @@
+package software.ulpgc.code.architecture.model
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
+import software.ulpgc.code.architecture.io.Store
+import kotlin.time.Clock
+
+class TaskMonitor(
+    private val store: Store,
+    private val checker: PeriodicTaskChecker,
+    private val renewer: PeriodicTaskRenewer
+) {
+    private val monitorScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+
+    init {
+        startMonitoring()
+    }
+    private fun startMonitoring() {
+        monitorScope.launch {
+            while (isActive) {
+                checkExpiredTasks()
+                delay(60_000)
+            }
+        }
+    }
+
+    private fun checkExpiredTasks() {
+        store.tasks()
+            .filterIsInstance<PeriodicTask>()
+            .filter { checker.needsRenewal(it) }
+            .forEach { task ->
+                checker.markAsDeleted(task)
+                renewer.renew(task)
+            }
+    }
+
+    fun dispose() {
+        monitorScope.cancel()
+    }
+}
